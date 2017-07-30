142bf7601470471f403ee6da2b0063309620abcb
Deepan <deepan.seeralan@gmail.com>
2017-07-30 12:13:44 -0400

    moved go files under separate directory and created go tests

---
+++ ctci/golang/chapter1/q2.go
@@ -0,0 +1,51 @@
+package chapter1
+
+// Given two strings, write a method to find whether one string
+// is a permutation of the other.
+
+func ArePermutations(str1, str2 string) bool {
+	if len(str1) != len(str2) {
+		return false
+	}
+
+	chars := make(map[rune]int)
+	for _, ch := range str1 {
+		chars[ch]++
+	}
+
+	for _, ch := range str2 {
+		if _, ok := chars[ch]; ok {
+			chars[ch]--
+			if chars[ch] < 0 {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+
+	return true
+}
+
+// type text struct {
+// 	val string
+// }
+
+// func (t text) isPermutationOf(otherText string) bool {
+// 	if len(t.val) != len(otherText) {
+// 		return false
+// 	}
+
+// 	s1 := strings.Split(t.val, "")
+// 	sort.Strings(s1)
+// 	s2 := strings.Split(otherText, "")
+// 	sort.Strings(s2)
+
+// 	return strings.Join(s1, "") == strings.Join(s2, "")
+// }
+
+// func main() {
+// 	t1 := text{"hellow"}
+// 	fmt.Println(t1.isPermutationOf("wohell"))
+// 	fmt.Println(t1.isPermutationOf("wohelowng"))
+// }
---
+++ ctci/golang/chapter1/q2_test.go
@@ -0,0 +1,27 @@
+package chapter1
+
+import (
+	"testing"
+)
+
+func TestArePermutations(t *testing.T) {
+	type testCase struct {
+		str1, str2 string
+		expected   bool
+	}
+	testCases := []testCase{testCase{"hellow", "wohell", true},
+		testCase{"jelly", "belly", false},
+		testCase{"equal", "unequal", false},
+		testCase{"neith92hgp", "jsdjtp1-4", false},
+		testCase{"anna", "nana", true},
+	}
+
+	for _, tCase := range testCases {
+		actual := ArePermutations(tCase.str1, tCase.str2)
+		if actual != tCase.expected {
+			t.Fatalf("Permutations computation failed for %s and %s, expected %t, actual %t\n",
+				tCase.str1, tCase.str2, tCase.expected, actual)
+		}
+	}
+
+}
